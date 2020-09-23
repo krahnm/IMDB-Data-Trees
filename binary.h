1d50897607571201cec799785e1f034322934737
krahnm <59669649+krahnm@users.noreply.github.com>
2020-09-23 18:11:36 -0400

    Update binary.h

--- binary.h
+++ binary.h
@@ -1,15 +1,2 @@
-
-/*void add_node(struct tree **root, char *string, struct title_basics *address);
-
-
-
-void add_nnode(struct tree **e, char *r, struct name_basics *t);
-struct title_basics *find_node(struct tree *q, char * w);
-*/
-
 struct tree *find_topNode(struct tree **root, char * toFind);
 struct title_principals *find_pnode(struct tree *root, char * toFind);
-/*void add_nnode(struct tree **root, char *string, struct name_basics *address);
- void add_tnode(struct tree **root, char *string, struct title_basics *address);*/
-/*void print_tree( struct tree *root, char *nConst, struct titleInfo *tInfo);
-*/
