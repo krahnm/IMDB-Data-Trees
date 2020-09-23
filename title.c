7c55897262b51be6f360206925d9f562caf6e53e
krahnm <59669649+krahnm@users.noreply.github.com>
2020-09-23 18:41:26 -0400

    Update title.c

--- title.c
+++ title.c
@@ -6,8 +6,10 @@
 #include "common.h"
 #include "title.h"
 
+/*Handles the array of movie title information*/
 
-struct titleInfo *get_title(char *path){
+struct titleInfo *get_title(char *path)
+{
 	struct titleInfo *treeInfo;
 	struct title_basics **nameArray = NULL;
 	
@@ -26,28 +28,19 @@ struct titleInfo *get_title(char *path){
 	strcpy(location, path);
 	strcat(location, "/title.basics.tsv");
 	
-	
+	/*Counts for only films labled Movie and creates the struct for the entry*/
 	fp = fopen(location, "r");
-	while (/*i<=700*/ !feof(fp)){
+	while (!feof(fp))
+	{
 		fgets(buffer, 256, fp);
-		/*if(strcmp(buffer,"NULL") != 0){
-			printf("\n BUFFER IS MESSED \n");
-		}*/
 		get_column(buffer, string1, 1);
 		get_column(buffer, string2, 4);
-		/*printf("Buffer	%s\n", buffer);
-		printf("Contents:	[%s]\n", string1);*/
 		if(strstr(string1, "movie")!=NULL && strstr(string2, "0")!=NULL){
 			lines++;
-			/*printf("LINES %d \n",lines);
-			 */
 		}
 		i++;
-		
 	}
 	
-	/*printf("\n Lines: %d \n",lines);
-	*/
 	nameArray = malloc(sizeof(struct title_basics)*lines);
 	treeInfo->numItems = lines;
 	treeInfo->value = nameArray;
@@ -56,14 +49,14 @@ struct titleInfo *get_title(char *path){
 	fseek(fp, 0, SEEK_SET);
 	i=0;
 	k = 0;
-	while (/*k<=700*/ !feof(fp)){ 
-		
+	while (!feof(fp))/*Copies the info from the file into the struct*/
+	{ 
 		fgets(buffer, 256, fp);
 		get_column(buffer, string1, 1);
 		get_column(buffer, string2, 4);
-		/*printf("Contents:	[%s]\n", string2);
-		*/
-		if(strstr(string1, "movie")!=NULL && strstr(string2, "0")!=NULL){
+	
+		if(strstr(string1, "movie")!=NULL && strstr(string2, "0")!=NULL)
+		{
 			get_column(buffer, string1, 0);
 			tConst = malloc(strlen(string1)+1);
 			strcpy(tConst, string1);
@@ -72,27 +65,13 @@ struct titleInfo *get_title(char *path){
 			name = malloc(strlen(string1)+1);
 			strcpy(name, string1);
 			
-			
 			nameArray[i] = malloc(sizeof(struct title_basics));
 			nameArray[i]->tconst = tConst;
 			nameArray[i]->primaryTitle = name;
-			i++;
-			 
+			i++; 
 		}
 		k++;
 	}
-	/*int j=0;
-	for(j=0; j<=5; j++){
-		printf("nConst:	%s \n", nameArray[j]->nconst);
-	}
-	
-	for(j=0; j<=lines; j++){
-		free(nameArray[j]->nconst);
-		free(nameArray[j]->primaryName);
-		free(nameArray[j]);
-	}
-	free(nameArray);*/
-	
 	
 	fclose(fp);
 	free(location);
@@ -100,36 +79,41 @@ struct titleInfo *get_title(char *path){
 	free(string1);
 	free(string2);
 	
-	
 	return treeInfo;
 }
 
-
-
- void build_tindex(struct titleInfo *tInfo){
+/*builds the title name tree*/
+ void build_tindex(struct titleInfo *tInfo)
+ {
 	 int j = 0;
 	
-	for(j =0; j < tInfo->numItems; j++){
+	for(j =0; j < tInfo->numItems; j++)
+	{
 		 add_tnode(&tInfo->tindex, tInfo->value[j]->primaryTitle, tInfo->value[j]);
-	 }
+	}
 	
  }
  
- 
-struct title_basics *find_primary_title(struct titleInfo *tInfo, char * toFind){
+/*Returns the title node*/ 
+struct title_basics *find_primary_title(struct titleInfo *tInfo, char * toFind)
+{
 	return find_tnode(tInfo->tindex, toFind);
 }
 
- void build_tconstindex(struct titleInfo *tInfo){
-	 int j = 0;
-	for(j =0; j < tInfo->numItems; j++){
+/*Builds the tConst tree*/
+ void build_tconstindex(struct titleInfo *tInfo)
+ {
+	int j = 0;
+	for(j =0; j < tInfo->numItems; j++)
+	{
 		 add_tnode(&tInfo->nindex, reverse(tInfo->value[j]->tconst), tInfo->value[j]);
-	 }
+	}
 	
  }
  
- 
-struct title_basics *find_tConst(struct titleInfo *tInfo, char * toFind){
+/*finds the matching tConst node*/
+struct title_basics *find_tConst(struct titleInfo *tInfo, char * toFind)
+{
 	return find_tnode(tInfo->nindex, reverse(toFind));
 }
  
