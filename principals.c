50fa9d897bc43cc8d9544ebffd70f304187c49bd
krahnm <59669649+krahnm@users.noreply.github.com>
2020-09-23 18:34:14 -0400

    Update principals.c

--- principals.c
+++ principals.c
@@ -9,8 +9,10 @@
 #include "name.h"
 #include "title.h"
 
+/*This file Handles the array that connects the names to the titles*/
 
-struct principalsInfo *get_principals(char *path){
+struct principalsInfo *get_principals(char *path)
+{
 	struct principalsInfo *treeInfo;
 	struct title_principals **nameArray = NULL;
 	
@@ -25,49 +27,46 @@ struct principalsInfo *get_principals(char *path){
 	int lines = 0;
 	int i=0;
 	int k = 0;
+	
 	FILE *fp;
 	treeInfo = malloc(sizeof(struct principalsInfo));
 	strcpy(location, path);
 	strcat(location, "/title.principals.tsv");
 	
-	
+	/*Calculates number of entries needed and creates their structs*/
 	fp = fopen(location, "r");
-	while (/*i<=700 */ !feof(fp)){
+	while (!feof(fp))
+	{
 		fgets(buffer, 256, fp);
-		/*if(strcmp(buffer,"NULL") != 0){
-			printf("\n BUFFER IS MESSED \n");
-		}*/
 		get_column(buffer, string1, 3);
-		/*printf("Buffer	%s\n", buffer);
-		printf("Contents:	[%s]\n", string1);*/
-		if(strstr(string1, "actor")!=NULL){
-			lines++;
-			 
+		if(strstr(string1, "actor")!=NULL)
+		{
+			lines++; 
 		}
-		else if(strstr(string1, "actress")!=NULL){
+		else if(strstr(string1, "actress")!=NULL)
+		{
 			lines++;
 		}
 		i++;
 	}
 	
-	/*printf("\n Lines: %d \n",lines);
-	*/
-	/*lines = 5; TODO	DELETE AFTER TESTING!!!*/
 	nameArray = malloc(sizeof(struct title_principals)*lines);
 	treeInfo->numItems = lines;
 	treeInfo->value = nameArray;
 	treeInfo->tindex = 0;
 	treeInfo->nindex = 0;
 	
+	/*Reads in the actors nConst and tConst values to enter into the linking trees*/
 	fseek(fp, 0, SEEK_SET);
 	i=0;
 	k = 0;
-	while (/*k<=700*/ !feof(fp)){ 
-		
+	while (!feof(fp))
+	{ 
 		fgets(buffer, 256, fp);
 		get_column(buffer, string1, 3);
 		
-		if(strstr(string1, "actor")!=NULL || strstr(string1, "actress")!=NULL){
+		if(strstr(string1, "actor")!=NULL || strstr(string1, "actress")!=NULL)
+		{
 			get_column(buffer, string1, 2);
 			nConst = malloc(strlen(string1)+1);
 			strcpy(nConst, string1);
@@ -84,23 +83,10 @@ struct principalsInfo *get_principals(char *path){
 			nameArray[i]->nconst = nConst;
 			nameArray[i]->tconst = tConst;
 			nameArray[i]->characters = name;
-			i++;
-			 
+			i++; 
 		}
 		k++;
 	}
-	/*int j=0;
-	for(j=0; j<=5; j++){
-		printf("nConst:	%s \n", nameArray[j]->nconst);
-	}
-	
-	for(j=0; j<=lines; j++){
-		free(nameArray[j]->nconst);
-		free(nameArray[j]->primaryName);
-		free(nameArray[j]);
-	}
-	free(nameArray);*/
-	
 	
 	fclose(fp);
 	free(location);
@@ -109,32 +95,38 @@ struct principalsInfo *get_principals(char *path){
 	free(string2);
 	free(string3);
 	
-	
 	return treeInfo;
 }
 
-
- void build_tindex_tp(struct principalsInfo *nInfo){
+/*Creates the trees - the tConst*/
+ void build_tindex_tp(struct principalsInfo *nInfo)
+ {
 	 int j = 0;
-	 for(j =0; j < nInfo->numItems; j++){
+	 for(j =0; j < nInfo->numItems; j++)
+	 {
 		 add_ptnode(&nInfo->tindex, reverse(nInfo->value[j]->tconst), nInfo->value[j]);
 	 }
  }
  
- 
-struct title_principals *find_tconst_tp(struct principalsInfo *nInfo, char * toFind){
+/*given the name info, finds the associated tConst*/
+struct title_principals *find_tconst_tp(struct principalsInfo *nInfo, char * toFind)
+{
 	return find_pnode(nInfo->tindex, toFind);
 }
 
-void build_nindex_tp(struct principalsInfo *nInfo){
+/*Creates the trees - the tConst*/
+void build_nindex_tp(struct principalsInfo *nInfo)
+{
 	 int j = 0;
-	 for(j =0; j < nInfo->numItems; j++){
+	 for(j =0; j < nInfo->numItems; j++)
+	 {
 		 add_pnnode(&nInfo->nindex, nInfo->value[j]->nconst, nInfo->value[j]);
 	 }
  }
  
- 
-struct title_principals *find_nconst_tp(struct principalsInfo *nInfo, char * toFind){
+/*returns the node with the matching nConst*/
+struct title_principals *find_nconst_tp(struct principalsInfo *nInfo, char * toFind)
+{
 	return find_pnode(nInfo->nindex, toFind);
 }
 
